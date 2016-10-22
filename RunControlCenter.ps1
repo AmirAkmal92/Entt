493e15a8fe195fc3d11ea1879dac7ba9699ae558
erymuzuan <erymuzuan@hotmail.com>
2016-10-22 09:14:58 +0800

    Update script

--- RunControlCenter.ps1
+++ RunControlCenter.ps1
@@ -33,4 +33,54 @@ $machine = ($env:COMPUTERNAME).Replace("-","_")
 [System.Environment]::SetEnvironmentVariable("RX_POSENTT_BromConnectionString", "Data Source=S301\DEV2016;Initial Catalog=PittisNonCore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", "Process")
 [System.Environment]::SetEnvironmentVariable("RX_POSENTT_SnbWebNewAccount_BaseAddress", "http://eryken2.asuscomm.com:8086", "Process")
 
+if((Test-Path(".\version.json")) -eq $false)
+{
+    $updateJson = @"
+{   
+    "build": 10325,
+    "date" : "2016-10-20"
+}
+"@
+$updateJson | Out-File .\version.json -Encoding ascii
+}
+
+$json = Get-Content .\version.json | ConvertFrom-Json
+$build = $json.build
+Write-Host "Please wait while we check for new RX build, current build is $build" -ForegroundColor Cyan
+
+try{
+    $release = Invoke-WebRequest -Method Get -UseBasicParsing -Uri "http://eryken2.asuscomm.com:8080/binaries/$build.json" -ErrorAction Ignore
+    Write-Host $release.StatusCode
+
+    $jsonResponse = $release.Content | ConvertFrom-Json
+    $vnext = $jsonResponse.vnext;
+    # $updateScript = $jsonResponse["update-script"]
+    #download update script
+    Write-Host "There's a new update package, $vnext is available " -ForegroundColor Yellow
+    Write-Host "Do you want to apply this update[Yes(y), No (n)] : " -ForegroundColor Yellow -NoNewline
+ 
+    $applyUpdate = Read-Host
+    if($applyUpdate -eq 'y')
+    {        
+        if((Test-Path(".\$vnext")) -eq $true)
+        {
+            Remove-Item ".\$vnext" -Force -Recurse
+        }
+        mkdir ".\$vnext"
+        Invoke-WebRequest -Method Get -UseBasicParsing -Uri "http://eryken2.asuscomm.com:8080/binaries/$vnext/$vnext.ps1" -OutFile ".\$vnext\$vnext.ps1"
+
+        # run 
+        & ".\$vnext\$vnext.ps1"        
+    }
+
+
+}catch{
+ $code = $_.Exception.Response.StatusCode.Value__
+ if($code -eq 404){
+    Write-Host "No update is avalailable" -ForegroundColor Cyan
+    Write-Host "Starting control center"
+ }
+}
+
+
 & .\control.center\controlcenter.exe
