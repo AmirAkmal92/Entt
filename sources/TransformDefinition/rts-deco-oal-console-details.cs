ece1a517e82be2768ee18a910f1613135e33a969
erymuzuan <erymuzuan@hotmail.com>
2016-10-23 08:19:45 +0800

    Deco mapping #5135

---
+++ sources/TransformDefinition/rts-deco-oal-console-details.cs
@@ -0,0 +1,19 @@
+
+namespace Bespoke.PosEntt.Integrations.Transforms
+{
+    public partial class RtsDecoOalConsoleDetails
+    {
+
+
+        partial void BeforeTransform(Bespoke.PosEntt.Decos.Domain.Deco item, Bespoke.PosEntt.Adapters.Oal.dbo_console_details destination)
+        {
+
+        }
+
+        partial void AfterTransform(Bespoke.PosEntt.Decos.Domain.Deco item, Bespoke.PosEntt.Adapters.Oal.dbo_console_details destination)
+        {
+
+        }
+
+    }
+}
---
+++ sources/TransformDefinition/rts-deco-oal-delivery-console-event-new.cs
@@ -0,0 +1,19 @@
+
+namespace Bespoke.PosEntt.Integrations.Transforms
+{
+    public partial class RtsDecoOalDeliveryConsoleEventNew
+    {
+
+
+        partial void BeforeTransform(Bespoke.PosEntt.Decos.Domain.Deco item, Bespoke.PosEntt.Adapters.Oal.dbo_delivery_console_event_new destination)
+        {
+
+        }
+
+        partial void AfterTransform(Bespoke.PosEntt.Decos.Domain.Deco item, Bespoke.PosEntt.Adapters.Oal.dbo_delivery_console_event_new destination)
+        {
+
+        }
+
+    }
+}
