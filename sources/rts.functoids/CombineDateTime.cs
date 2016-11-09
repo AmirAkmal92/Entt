5626ad8efd40adc08a3feb5077ede106bc3ee2a0
erymuzuan <erymuzuan@hotmail.com>
2016-11-09 17:57:35 +0800

    custom functoid

---
+++ sources/rts.functoids/CombineDateTime.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using Bespoke.Sph.Domain;
+
+namespace rts.functoids
+{
+    [Export("FunctoidDesigner", typeof(Functoid))]
+    [DesignerMetadata(Name = "CombineDateTime", BootstrapIcon = "calendar", Category = FunctoidCategory.DATE)]
+    public class CombineDateTime : Functoid
+    {
+        public override string GenerateAssignmentCode()
+        {
+            return @"new DateTime(item.Date.Year, item.Date.Month, item.Date.Day, item.Time.Hour, item.Time.Minute, item.Time.Second)";
+        }
+    }
+}
