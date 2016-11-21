935503e79c6150638a77433679e394b65a4dee46
erymuzuan <erymuzuan@hotmail.com>
2016-11-22 06:00:41 +0800

    Date range filter

--- web/SphApp/viewmodels/rts-messages.js
+++ web/SphApp/viewmodels/rts-messages.js
@@ -8,7 +8,17 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
         total = ko.observable(),
         from = ko.observable(0),
         size = ko.observable(20),
+        dateFrom = ko.observable(moment().subtract(1, "month").format()),
+        to = ko.observable(moment().endOf("day").format()),
         query = {
+            "query": {
+                "range": {
+                    "CreatedDate": {
+                        "from": dateFrom,
+                        "to": to
+                    }
+                }
+            },
             "from": from,
             "size": size,
             "sort": [
@@ -48,6 +58,34 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
             });
 
             total.subscribe(pager.update);
+
+            $(view).find("div.date-range")
+               .daterangepicker(
+                   {
+                       ranges: {
+                           'Today': [moment().startOf("day"), moment().endOf("day")],
+                           'Yesterday': [moment().startOf("day").subtract(1, "days"), moment().endOf("day").subtract(1, "days")],
+                           'Last 7 Days': [moment().startOf("day").subtract(6, "days"), moment()],
+                           'Last 30 Days': [moment().startOf("day").subtract(29, "days"), moment()],
+                           'This Month': [moment().startOf("day").startOf("month"), moment().endOf("month")],
+                           'Last Month': [
+                               moment().subtract(1, "month").startOf("month"),
+                               moment().subtract(1, "month").endOf("month")
+                           ]
+                       },
+                       startDate: moment().subtract(29, "days"),
+                       endDate: moment()
+                   },
+                   function (start, end) {
+                       dateFrom(start.format());
+                       to(end.format());
+                       return context.post(ko.toJSON(query), `api/rts-dashboard/${rtsType().toLowerCase()}`)
+                          .then(function (result) {
+                              total(result.hits.total);
+                              list(result.hits.hits);
+                          });
+                   }
+               );
         },
         requeue = function() {
             return context.post(ko.toJSON(query), `api/rts-dashboard/${rtsType()}`)
@@ -66,6 +104,8 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
         queueOptions: queueOptions,
         requeue: requeue,
         queue: queue,
+        dateFrom: dateFrom,
+        to: to,
         isBusy: isBusy
     };
 
