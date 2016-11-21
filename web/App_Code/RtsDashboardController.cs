c5845e0325b97e1ac86c78bbe811dccca40e620d
erymuzuan <erymuzuan@hotmail.com>
2016-11-22 05:50:50 +0800

    list for rts messages

--- web/App_Code/RtsDashboardController.cs
+++ web/App_Code/RtsDashboardController.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Linq;
 using System.Net.Http;
-using System.ServiceModel.Channels;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
-using System.Web.Security;
 using Bespoke.Sph.Domain;
 using Bespoke.Sph.WebApi;
-
 
 
 [RoutePrefix("api/rts-dashboard")]
@@ -16,16 +11,27 @@ public class RtsDashboadController : BaseApiController
 {
 
     private readonly HttpClient m_client = new HttpClient{BaseAddress = new Uri( ConfigurationManager.ElasticSearchHost)};
-    public RtsDashboadController()
-    {
-
-    }
-
+   
     [Route("")]
     [HttpPost]
     public async Task<IHttpActionResult> Aggregates([RawBody]string query)
     {
         var response = await m_client.PostAsync($"{ConfigurationManager.ElasticSearchIndex}_rts/_search", new StringContent(query));
+        var content = response.Content as StreamContent;
+        if (null == content) throw new Exception("Cannot execute query on es ");
+        var result = await content.ReadAsStringAsync();
+
+        if (!response.IsSuccessStatusCode)
+            throw new Exception($"Cannot execute query for :  {query}, Result = {result} ");
+        return Json(result);
+    }
+
+    [Route("{type}")]
+    [HttpPost]
+    public async Task<IHttpActionResult> SearchAsync(string type, 
+        [RawBody]string query)
+    {
+        var response = await m_client.PostAsync($"{ConfigurationManager.ElasticSearchIndex}_rts/{type.ToLowerInvariant()}/_search", new StringContent(query));
         var content = response.Content as StreamContent;
         if (null == content) throw new Exception("Cannot execute query on es ");
         var result = await content.ReadAsStringAsync();
--- web/SphApp/viewmodels/rts-messages.js
+++ web/SphApp/viewmodels/rts-messages.js
@@ -1,18 +1,72 @@
-define(["services/datacontext", "services/logger", "plugins/router", objectbuilders.system, objectbuilders.app], function(context, logger, router, system, app){
-    var isBusy = ko.observable(false),
+define(["services/datacontext", "services/logger", "plugins/router", objectbuilders.system, objectbuilders.app], function (context, logger, router, system, app) {
+    const isBusy = ko.observable(false),
+        list = ko.observableArray(),
+        queueOptions = ko.observableArray(),
+        selectedItems = ko.observableArray(),
+        queue = ko.observable(),
         rtsType = ko.observable(),
-        activate = function(type){
-            rtsType(type);
-            return true;
+        total = ko.observable(),
+        from = ko.observable(0),
+        size = ko.observable(20),
+        query = {
+            "from": from,
+            "size": size,
+            "sort": [
+               {
+                   "CreatedDate": {
+                       "order": "desc"
+                   }
+               }
+            ]
         },
-        attached  = function(view){
-        
+        activate = function (type) {
+            rtsType(type);
+            return context.post(ko.toJSON(query), `api/rts-dashboard/${type.toLowerCase()}`)
+                .then(function (result) {
+                    total(result.hits.total);
+                    list(result.hits.hits);
+
+                    return context.loadAsync("Trigger", `Entity eq '${type}'`);
+                }).then(function(lo) {
+                    queueOptions(lo.itemCollection);
+                });
+        },
+        attached = function (view) {
+            const element = $(view).find("div#messages-pager");
+            const pager = new bespoke.utils.ServerPager({
+                count: ko.unwrap(total),
+                element: element,
+                changed: function (page, ps) {
+                    from(ps * (page - 1));
+                    size(ps);
+                    return context.post(ko.toJSON(query), `api/rts-dashboard/${rtsType().toLowerCase()}`)
+                      .then(function (result) {
+                          total(result.hits.total);
+                          list(result.hits.hits);
+                      });
+                }
+            });
+
+            total.subscribe(pager.update);
+        },
+        requeue = function() {
+            return context.post(ko.toJSON(query), `api/rts-dashboard/${rtsType()}`)
+                    .then(function (result) {
+                        total(result.hits.total);
+                        list(result.hits.hits);
+                    });
         };
 
     return {
-        activate : activate,
-        attached : attached,
-        isBusy : isBusy
+        activate: activate,
+        attached: attached,
+        type: rtsType,
+        list: list,
+        selectedItems: selectedItems,
+        queueOptions: queueOptions,
+        requeue: requeue,
+        queue: queue,
+        isBusy: isBusy
     };
 
 });
