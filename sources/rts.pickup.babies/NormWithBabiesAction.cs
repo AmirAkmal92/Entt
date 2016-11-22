82bea0946a021586bee69ed1bc9a5ea77feb67e8
azrolmn <azrolmn@gmail.com>
2016-11-22 22:30:52 +0800

    default norm children items

---
+++ sources/rts.pickup.babies/NormWithBabiesAction.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+using Polly;
+
+namespace Bespoke.PosEntt.CustomActions
+{
+    [Export(typeof(CustomAction))]
+    [DesignerMetadata(Name = "NormWithBabies", TypeName = "Bespoke.PosEntt.CustomActions.NormWithBabiesAction, rts.pickup.babies", Description = "RTS Norm with babies", FontAwesomeIcon = "calendar-check-o")]
+    public class NormWithBabiesAction : CustomAction
+    {
+        public override bool UseAsync => true;
+        public override async Task ExecuteAsync(RuleContext context)
+        {
+            var norm = context.Item as Norms.Domain.Norm;
+            if (null == norm) return;
+            if (norm.AllConsignmentNotes.Length <= 0) return;
+            await RunAsync(norm);
+        }
+
+        public async Task RunAsync(Norms.Domain.Norm norm)
+        {
+            //norm console_details
+            var normEventMap = new Integrations.Transforms.RtsNormToNormalConsoleEventNew();
+            var normEventRows = new List<Adapters.Oal.dbo_normal_console_event_new>();
+            var parentRow = await normEventMap.TransformAsync(norm);
+            normEventRows.Add(parentRow);
+
+            var itemList = norm.AllConsignmentNotes.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in itemList)
+            {
+                var childItem = parentRow.Clone();
+                childItem.id = GenerateId(20);
+                childItem.consignment_no = item;
+                childItem.item_type_code = IsConsole(item) ? "02" : "01";
+                normEventRows.Add(childItem);
+            }
+
+            var normEventAdapter = new Adapters.Oal.dbo_normal_console_event_newAdapter();
+            foreach (var item in normEventRows)
+            {
+                var pr = Policy.Handle<SqlException>()
+                    .WaitAndRetryAsync(5, x => TimeSpan.FromMilliseconds(500 * Math.Pow(2, x)))
+                    .ExecuteAndCaptureAsync(() => normEventAdapter.InsertAsync(item));
+                var result = await pr;
+                if (result.FinalException != null)
+                    throw result.FinalException; // send to dead letter queue
+                System.Diagnostics.Debug.Assert(result.Result > 0, "Should be at least 1 row");
+            }            
+        }
+
+        public override string GetEditorViewModel()
+        {
+            return @"
+define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
+    function(context, logger, dialog, system) {
+
+                bespoke.sph.domain.NormWithBabiesAction = function(optionOrWebid) {
+
+                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
+                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.NormWithBabiesAction, rts.pickup.babies"";
+                    if (optionOrWebid && typeof optionOrWebid === ""object"")
+                    {
+                        for (var n in optionOrWebid)
+                        {
+                            if (typeof v[n] === ""function"")
+                            {
+                                v[n](optionOrWebid[n]);
+                            }
+                        }
+                    }
+                    if (optionOrWebid && typeof optionOrWebid === ""string"")
+                    {
+                        v.WebId(optionOrWebid);
+                    }
+
+                    if (bespoke.sph.domain.NormWithBabiesActionPartial)
+                    {
+                        return _(v).extend(new bespoke.sph.domain.NormWithBabiesActionPartial(v));
+                    }
+                    return v;
+                };
+
+
+
+        const action = ko.observable(new bespoke.sph.domain.NormWithBabiesAction(system.guid())),
+            trigger = ko.observable(),                   
+            activate = function() {
+                   return true;
+
+                },
+            attached = function(view) {
+                },
+            okClick = function(data, ev) {
+                    if (bespoke.utils.form.checkValidity(ev.target))
+                    {
+                        dialog.close(this, ""OK"");
+                    }
+                },
+            cancelClick = function() {
+                    dialog.close(this, ""Cancel"");
+                };
+
+                const vm = {
+                    trigger: trigger,
+                    action: action,
+                    activate: activate,
+                    attached: attached,
+                    okClick: okClick,
+                    cancelClick: cancelClick
+                };
+
+
+            return vm;
+
+        });
+";
+        }
+
+        public override string GetEditorView()
+        {
+            //language=html
+            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
+    <div class=""modal-dialog"">
+        <div class=""modal-content"">
+
+            <div class=""modal-header"">
+                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
+                <h3>RTS norm with babies</h3>
+            </div>
+            <div class=""modal-body"" data-bind=""with: action"">
+
+                <form class=""form-horizontal"" id=""messaging-dialog-form"">
+
+
+                </form>
+            </div>
+            <div class=""modal-footer"">
+                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
+                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
+            </div>
+        </div>
+    </div>
+</section>";
+
+            return html;
+        }
+
+        private string GenerateId(int length)
+        {
+            var ticks = System.DateTime.Now.Ticks;
+            var id = string.Format("en{0}{1}", ticks.ToString().Substring(6), System.Guid.NewGuid().ToString("N"));
+            return id.Substring(0, length);
+        }
+
+        private bool IsConsole(string connoteNo)
+        {
+            var pattern = "CG[0-9]{9}MY";
+            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, pattern);
+            return match.Success;
+        }
+    }
+}
