90742d4b03bd9dbf253b88ba50ae6bbdb5a9bf59
erymuzuan <erymuzuan@hotmail.com>
2016-12-07 05:19:12 +0800

    Use base class EventWithChildAction for shared methods
    Related Work Items: #5376

--- sources/rts.pickup.babies/DeliWithChildrenAction.cs
+++ sources/rts.pickup.babies/DeliWithChildrenAction.cs
@@ -5,16 +5,13 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
 using Polly;
-using System.Linq;
 
 namespace Bespoke.PosEntt.CustomActions
 {
     [Export(typeof(CustomAction))]
-    [DesignerMetadata(Name = "DeliWithChildren", TypeName = "Bespoke.PosEntt.CustomActions.DeliWithChildrenAction, rts.pickup.babies", Description = "RTS DELI with child items", FontAwesomeIcon = "calendar-check-o")]
-    public class DeliWithChildrenAction : CustomAction
+    [DesignerMetadata(Name = "DeliWithChildren", TypeName = "Bespoke.PosEntt.CustomActions.DeliWithChildrenAction, rts.pickup.babies", Description = "RTS DELI with child items", FontAwesomeIcon = "car")]
+    public class DeliWithChildrenAction : EventWithChildrenAction
     {
-        public override bool UseAsync => true;
-
         private List<Adapters.Oal.dbo_delivery_event_new> m_deliEventRows;
         private List<Adapters.Oal.dbo_wwp_event_new_log> m_deliWwpEventLogRows;
         private List<Adapters.Oal.dbo_event_pending_console> m_deliEventPendingConsoleRows;
@@ -50,10 +47,10 @@ namespace Bespoke.PosEntt.CustomActions
             var parentWwpRow = await deliWwpEventLogMap.TransformAsync(deli);
             m_deliWwpEventLogRows.Add(parentWwpRow);
 
-            var consoleItem = await SearchConsoleDetails(deli.ConsignmentNo);
+            var consoleItem = await GetItemConsigmentsFromConsoleDetailsAsync(deli.ConsignmentNo);
             if (null != consoleItem)
             {
-                var children = consoleItem.item_consignments.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var children = consoleItem.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var item in children)
                 {
                     if (consoleList.Contains(item)) continue;
@@ -65,10 +62,10 @@ namespace Bespoke.PosEntt.CustomActions
                     if (console)
                     {
                         consoleList.Add(item);
-                        var childConsole = await SearchConsoleDetails(item);
+                        var childConsole = await GetItemConsigmentsFromConsoleDetailsAsync(item);
                         if (null != childConsole)
                         {
-                            var childConsoleItems = childConsole.item_consignments.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                            var childConsoleItems = childConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                             foreach (var cc in childConsoleItems)
                             {
                                 if (consoleList.Contains(cc)) continue;
@@ -80,10 +77,10 @@ namespace Bespoke.PosEntt.CustomActions
                                 if (anotherConsole)
                                 {
                                     consoleList.Add(cc);
-                                    var anotherChildConsole = await SearchConsoleDetails(cc);
+                                    var anotherChildConsole = await GetItemConsigmentsFromConsoleDetailsAsync(cc);
                                     if (null != anotherChildConsole)
                                     {
-                                        var anotherChildConsoleItems = anotherChildConsole.item_consignments.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                        var anotherChildConsoleItems = anotherChildConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                                         foreach (var ccc in anotherChildConsoleItems)
                                         {
                                             if (consoleList.Contains(ccc)) continue;
@@ -145,14 +142,6 @@ namespace Bespoke.PosEntt.CustomActions
             }
         }
 
-        private async Task<Adapters.Oal.dbo_console_details> SearchConsoleDetails(string consignmentNo)
-        {
-            var consoleDetailsAdapter = new Adapters.Oal.dbo_console_detailsAdapter();
-            var query = $"SELECT * FROM [dbo].[console_details] WHERE console_no = '{consignmentNo}'";
-            var lo = await consoleDetailsAdapter.LoadAsync(query);
-            return lo.ItemCollection.FirstOrDefault();
-        }
-
         private void ProcessChild(Adapters.Oal.dbo_delivery_event_new parent, string consignmentNo)
         {
             var console = IsConsole(consignmentNo);
@@ -198,114 +187,6 @@ namespace Bespoke.PosEntt.CustomActions
             m_deliWwpEventLogRows.Add(wwp);
         }
 
-        public override string GetEditorViewModel()
-        {
-            return @"
-define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
-    function(context, logger, dialog, system) {
 
-                bespoke.sph.domain.DeliWithChildrenAction = function(optionOrWebid) {
-
-                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
-                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.DeliWithChildrenAction, rts.pickup.babies"";
-                    if (optionOrWebid && typeof optionOrWebid === ""object"")
-                    {
-                        for (var n in optionOrWebid)
-                        {
-                            if (typeof v[n] === ""function"")
-                            {
-                                v[n](optionOrWebid[n]);
-                            }
-                        }
-                    }
-                    if (optionOrWebid && typeof optionOrWebid === ""string"")
-                    {
-                        v.WebId(optionOrWebid);
-                    }
-
-                    if (bespoke.sph.domain.DeliWithChildrenActionPartial)
-                    {
-                        return _(v).extend(new bespoke.sph.domain.DeliWithChildrenActionPartial(v));
-                    }
-                    return v;
-                };
-
-
-
-        const action = ko.observable(new bespoke.sph.domain.DeliWithChildrenAction(system.guid())),
-            trigger = ko.observable(),                   
-            activate = function() {
-                   return true;
-
-                },
-            attached = function(view) {
-                },
-            okClick = function(data, ev) {
-                    if (bespoke.utils.form.checkValidity(ev.target))
-                    {
-                        dialog.close(this, ""OK"");
-                    }
-                },
-            cancelClick = function() {
-                    dialog.close(this, ""Cancel"");
-                };
-
-                const vm = {
-                    trigger: trigger,
-                    action: action,
-                    activate: activate,
-                    attached: attached,
-                    okClick: okClick,
-                    cancelClick: cancelClick
-                };
-
-
-            return vm;
-
-        });
-";
-        }
-
-        public override string GetEditorView()
-        {
-            //language=html
-            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
-    <div class=""modal-dialog"">
-        <div class=""modal-content"">
-
-            <div class=""modal-header"">
-                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
-                <h3>RTS DELI with child items</h3>
-            </div>
-            <div class=""modal-body"" data-bind=""with: action"">
-
-                <form class=""form-horizontal"" id=""messaging-dialog-form"">
-
-
-                </form>
-            </div>
-            <div class=""modal-footer"">
-                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
-                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
-            </div>
-        </div>
-    </div>
-</section>";
-
-            return html;
-        }
-
-        private static string GenerateId(int length)
-        {
-            var id = $"en{Guid.NewGuid():N}";
-            return id.Substring(0, length);
-        }
-
-        private static bool IsConsole(string connoteNo)
-        {
-            const string PATTERN = "CG[0-9]{9}MY";
-            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, PATTERN);
-            return match.Success;
-        }
     }
 }
---
+++ sources/rts.pickup.babies/EventWithChildrenAction.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using Bespoke.Sph.Domain;
+
+namespace Bespoke.PosEntt.CustomActions
+{
+    public class EventWithChildrenAction : CustomAction
+    {
+        public override bool UseAsync => true;
+
+        protected Task<string> GetItemConsigmentsFromConsoleDetailsAsync(string consignmentNo)
+        {
+            var consoleDetailsAdapter = new Adapters.Oal.dbo_console_detailsAdapter();
+            var query = $"SELECT [item_consignments] FROM [dbo].[console_details] WHERE console_no = '{consignmentNo}'";
+            return consoleDetailsAdapter.ExecuteScalarAsync<string>(query);
+            
+        }
+
+        public override string GetEditorViewModel()
+        {
+            return @"
+define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
+    function(context, logger, dialog, system) {
+
+                bespoke.sph.domain.DeliWithChildrenAction = function(optionOrWebid) {
+
+                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
+                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.DeliWithChildrenAction, rts.pickup.babies"";
+                    if (optionOrWebid && typeof optionOrWebid === ""object"")
+                    {
+                        for (var n in optionOrWebid)
+                        {
+                            if (typeof v[n] === ""function"")
+                            {
+                                v[n](optionOrWebid[n]);
+                            }
+                        }
+                    }
+                    if (optionOrWebid && typeof optionOrWebid === ""string"")
+                    {
+                        v.WebId(optionOrWebid);
+                    }
+
+                    if (bespoke.sph.domain.DeliWithChildrenActionPartial)
+                    {
+                        return _(v).extend(new bespoke.sph.domain.DeliWithChildrenActionPartial(v));
+                    }
+                    return v;
+                };
+
+
+
+        const action = ko.observable(new bespoke.sph.domain.DeliWithChildrenAction(system.guid())),
+            trigger = ko.observable(),                   
+            activate = function() {
+                   return true;
+
+                },
+            attached = function(view) {
+                },
+            okClick = function(data, ev) {
+                    if (bespoke.utils.form.checkValidity(ev.target))
+                    {
+                        dialog.close(this, ""OK"");
+                    }
+                },
+            cancelClick = function() {
+                    dialog.close(this, ""Cancel"");
+                };
+
+                const vm = {
+                    trigger: trigger,
+                    action: action,
+                    activate: activate,
+                    attached: attached,
+                    okClick: okClick,
+                    cancelClick: cancelClick
+                };
+
+
+            return vm;
+
+        });
+";
+        }
+
+        public override string GetEditorView()
+        {
+            //language=html
+            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
+    <div class=""modal-dialog"">
+        <div class=""modal-content"">
+
+            <div class=""modal-header"">
+                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
+                <h3>RTS DELI with child items</h3>
+            </div>
+            <div class=""modal-body"" data-bind=""with: action"">
+
+                <form class=""form-horizontal"" id=""messaging-dialog-form"">
+
+
+                </form>
+            </div>
+            <div class=""modal-footer"">
+                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
+                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
+            </div>
+        </div>
+    </div>
+</section>";
+
+            return html;
+        }
+
+        protected static string GenerateId(int length)
+        {
+            var id = $"en{Guid.NewGuid():N}";
+            return id.Substring(0, length);
+        }
+
+        protected static bool IsConsole(string connoteNo)
+        {
+            const string PATTERN = "CG[0-9]{9}MY";
+            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, PATTERN);
+            return match.Success;
+        }
+    }
+}
