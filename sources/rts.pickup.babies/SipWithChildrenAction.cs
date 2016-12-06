88bce7a76aefabd3ca2a06aa3027a9773d9ede74
erymuzuan <erymuzuan@hotmail.com>
2016-12-07 05:23:07 +0800

    use EventWithChildrenAction base class
    Related Work Items: #5377

--- sources/rts.pickup.babies/SipWithChildrenAction.cs
+++ sources/rts.pickup.babies/SipWithChildrenAction.cs
@@ -10,9 +10,8 @@ namespace Bespoke.PosEntt.CustomActions
 {
     [Export(typeof(CustomAction))]
     [DesignerMetadata(Name = "SipWithChildren", TypeName = "Bespoke.PosEntt.CustomActions.SipWithChildrenAction, rts.pickup.babies", Description = "RTS SIP with child items", FontAwesomeIcon = "calendar-check-o")]
-    public class SipWithChildrenAction : CustomAction
+    public class SipWithChildrenAction : EventWithChildrenAction
     {
-        public override bool UseAsync => true;
 
         private List<Adapters.Oal.dbo_sip_event_new> m_sipEventRows;
         private List<Adapters.Oal.dbo_wwp_event_new_log> m_sipWwpEventLogRows;
@@ -144,13 +143,7 @@ namespace Bespoke.PosEntt.CustomActions
             }
         }
 
-        private static Task<string> GetItemConsigmentsFromConsoleDetailsAsync(string consignmentNo)
-        {
-            var consoleDetailsAdapter = new Adapters.Oal.dbo_console_detailsAdapter();
-            var query = $"SELECT [item_consignments] FROM [dbo].[console_details] WHERE console_no = '{consignmentNo}'";
-            return consoleDetailsAdapter.ExecuteScalarAsync<string>(query);
-        }
-
+   
         private void ProcessChild(Adapters.Oal.dbo_sip_event_new parent, string consignmentNo)
         {
             var console = IsConsole(consignmentNo);
@@ -195,115 +188,8 @@ namespace Bespoke.PosEntt.CustomActions
             wwp.consignment_note_number = childConnoteNo;
             m_sipWwpEventLogRows.Add(wwp);
         }
-
-        public override string GetEditorViewModel()
-        {
-            return @"
-define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
-    function(context, logger, dialog, system) {
-
-                bespoke.sph.domain.SipWithChildrenAction = function(optionOrWebid) {
-
-                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
-                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.SipWithChildrenAction, rts.pickup.babies"";
-                    if (optionOrWebid && typeof optionOrWebid === ""object"")
-                    {
-                        for (var n in optionOrWebid)
-                        {
-                            if (typeof v[n] === ""function"")
-                            {
-                                v[n](optionOrWebid[n]);
-                            }
-                        }
-                    }
-                    if (optionOrWebid && typeof optionOrWebid === ""string"")
-                    {
-                        v.WebId(optionOrWebid);
-                    }
-
-                    if (bespoke.sph.domain.SipWithChildrenActionPartial)
-                    {
-                        return _(v).extend(new bespoke.sph.domain.SipWithChildrenActionPartial(v));
-                    }
-                    return v;
-                };
-
-
-
-        const action = ko.observable(new bespoke.sph.domain.SipWithChildrenAction(system.guid())),
-            trigger = ko.observable(),                   
-            activate = function() {
-                   return true;
-
-                },
-            attached = function(view) {
-                },
-            okClick = function(data, ev) {
-                    if (bespoke.utils.form.checkValidity(ev.target))
-                    {
-                        dialog.close(this, ""OK"");
-                    }
-                },
-            cancelClick = function() {
-                    dialog.close(this, ""Cancel"");
-                };
-
-                const vm = {
-                    trigger: trigger,
-                    action: action,
-                    activate: activate,
-                    attached: attached,
-                    okClick: okClick,
-                    cancelClick: cancelClick
-                };
-
-
-            return vm;
-
-        });
-";
-        }
-
-        public override string GetEditorView()
-        {
-            //language=html
-            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
-    <div class=""modal-dialog"">
-        <div class=""modal-content"">
-
-            <div class=""modal-header"">
-                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
-                <h3>RTS SIP with child items</h3>
-            </div>
-            <div class=""modal-body"" data-bind=""with: action"">
-
-                <form class=""form-horizontal"" id=""messaging-dialog-form"">
-
-
-                </form>
-            </div>
-            <div class=""modal-footer"">
-                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
-                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
-            </div>
-        </div>
-    </div>
-</section>";
-
-            return html;
-        }
-
-        private string GenerateId(int length)
-        {
-            var id = $"en{Guid.NewGuid():N}";
-            return id.Substring(0, length);
-        }
-
-        private bool IsConsole(string connoteNo)
-        {
-            const string PATTERN = "CG[0-9]{9}MY";
-            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, PATTERN);
-            return match.Success;
-        }
+        
+        
+        
     }
 }
