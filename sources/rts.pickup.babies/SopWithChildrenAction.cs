badddfd27788c69c47b3cc98953e1986e5f7624d
erymuzuan <erymuzuan@hotmail.com>
2016-12-07 05:26:20 +0800

    Use EventWithChildrenAction, fixed the paging bugs
    Related Work Items: #5374

--- sources/rts.pickup.babies/SopWithChildrenAction.cs
+++ sources/rts.pickup.babies/SopWithChildrenAction.cs
@@ -5,15 +5,13 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
 using Polly;
-using System.Linq;
 
 namespace Bespoke.PosEntt.CustomActions
 {
     [Export(typeof(CustomAction))]
     [DesignerMetadata(Name = "SopWithChildren", TypeName = "Bespoke.PosEntt.CustomActions.SopWithChildrenAction, rts.pickup.babies", Description = "RTS SOP with child items", FontAwesomeIcon = "calendar-check-o")]
-    public class SopWithChildrenAction : CustomAction
+    public class SopWithChildrenAction : EventWithChildrenAction
     {
-        public override bool UseAsync => true;
 
         private List<Adapters.Oal.dbo_sop_event_new> m_sopEventRows;
         private List<Adapters.Oal.dbo_wwp_event_new_log> m_sopWwpEventLogRows;
@@ -30,15 +28,14 @@ namespace Bespoke.PosEntt.CustomActions
             m_sopWwpEventLogRows = new List<Adapters.Oal.dbo_wwp_event_new_log>();
             m_sopEventPendingConsoleRows = new List<Adapters.Oal.dbo_event_pending_console>();
 
-        await RunAsync(sop);
+            await RunAsync(sop);
         }
 
         public async Task RunAsync(Sops.Domain.Sop sop)
-        {            
+        {
 
             //console_details
-            var consoleList = new List<string>();
-            consoleList.Add(sop.ConsignmentNo);
+            var consoleList = new List<string> { sop.ConsignmentNo };
 
             var sopWwpEventAdapter = new Adapters.Oal.dbo_wwp_event_new_logAdapter();
             var sopEventAdapter = new Adapters.Oal.dbo_sop_event_newAdapter();
@@ -51,10 +48,10 @@ namespace Bespoke.PosEntt.CustomActions
             var parentWwpRow = await sopWwpEventLogMap.TransformAsync(sop);
             m_sopWwpEventLogRows.Add(parentWwpRow);
 
-            var consoleItem = await SearchConsoleDetails(sop.ConsignmentNo);
+            var consoleItem = await GetItemConsigmentsFromConsoleDetailsAsync(sop.ConsignmentNo);
             if (null != consoleItem)
             {
-                var children = consoleItem.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var children = consoleItem.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var item in children)
                 {
                     if (consoleList.Contains(item)) continue;
@@ -63,48 +60,44 @@ namespace Bespoke.PosEntt.CustomActions
 
                     //2 level
                     var console = IsConsole(item);
-                    if (console)
+                    if (!console) continue;
+                    consoleList.Add(item);
+                    var childConsole = await GetItemConsigmentsFromConsoleDetailsAsync(item);
+                    if (null != childConsole)
                     {
-                        consoleList.Add(item);
-                        var childConsole = await SearchConsoleDetails(item);
-                        if (null != childConsole)
+                        var childConsoleItems = childConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                        foreach (var cc in childConsoleItems)
                         {
-                            var childConsoleItems = childConsole.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                            foreach (var cc in childConsoleItems)
-                            {
-                                if (consoleList.Contains(cc)) continue;
-                                ProcessChild(parentRow, cc);
-                                ProcessChildWwp(parentWwpRow, cc);
+                            if (consoleList.Contains(cc)) continue;
+                            ProcessChild(parentRow, cc);
+                            ProcessChildWwp(parentWwpRow, cc);
 
-                                //3 level
-                                var anotherConsole = IsConsole(cc);
-                                if (anotherConsole)
+                            //3 level
+                            var anotherConsole = IsConsole(cc);
+                            if (!anotherConsole) continue;
+                            consoleList.Add(cc);
+                            var anotherChildConsole = await GetItemConsigmentsFromConsoleDetailsAsync(cc);
+                            if (null != anotherChildConsole)
+                            {
+                                var anotherChildConsoleItems = anotherChildConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                foreach (var ccc in anotherChildConsoleItems)
                                 {
-                                    consoleList.Add(cc);
-                                    var anotherChildConsole = await SearchConsoleDetails(cc);
-                                    if (null != anotherChildConsole)
-                                    {
-                                        var anotherChildConsoleItems = anotherChildConsole.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                                        foreach (var ccc in anotherChildConsoleItems)
-                                        {
-                                            if (consoleList.Contains(ccc)) continue;
-                                            ProcessChild(parentRow, ccc);
-                                            ProcessChildWwp(parentWwpRow, ccc);
-                                        }
-                                    }
-                                    else
-                                    {
-                                        AddPendingItems(parentRow.id, parentWwpRow.id, cc);
-                                    }
+                                    if (consoleList.Contains(ccc)) continue;
+                                    ProcessChild(parentRow, ccc);
+                                    ProcessChildWwp(parentWwpRow, ccc);
                                 }
                             }
-                        }
-                        else
-                        {
-                            AddPendingItems(parentRow.id, parentWwpRow.id, item);
+                            else
+                            {
+                                AddPendingItems(parentRow.id, parentWwpRow.id, cc);
+                            }
                         }
                     }
-                }                
+                    else
+                    {
+                        AddPendingItems(parentRow.id, parentWwpRow.id, item);
+                    }
+                }
             }
             else
             {
@@ -148,14 +141,6 @@ namespace Bespoke.PosEntt.CustomActions
             }
         }
 
-        async private Task<Adapters.Oal.dbo_console_details> SearchConsoleDetails(string consignmentNo)
-        {
-            var consoleDetailsAdapter = new Adapters.Oal.dbo_console_detailsAdapter();
-            var query = string.Format("SELECT * FROM [dbo].[console_details] WHERE console_no = '{0}'", consignmentNo);
-            var lo = await consoleDetailsAdapter.LoadAsync(query);
-            return lo.ItemCollection.FirstOrDefault();
-        }
-
         private void ProcessChild(Adapters.Oal.dbo_sop_event_new parent, string consignmentNo)
         {
             var console = IsConsole(consignmentNo);
@@ -170,22 +155,26 @@ namespace Bespoke.PosEntt.CustomActions
         private void AddPendingItems(string parentId, string wwpParentId, string consignmentNo)
         {
             //insert into pending items
-            var pendingConsole = new Adapters.Oal.dbo_event_pending_console();
-            pendingConsole.id = GenerateId(20);
-            pendingConsole.event_id = parentId;
-            pendingConsole.event_class = "pos.oal.SopEventNew";
-            pendingConsole.console_no = consignmentNo;
-            pendingConsole.version = 0;
-            pendingConsole.date_field = DateTime.Now;
+            var pendingConsole = new Adapters.Oal.dbo_event_pending_console
+            {
+                id = GenerateId(20),
+                event_id = parentId,
+                event_class = "pos.oal.SopEventNew",
+                console_no = consignmentNo,
+                version = 0,
+                date_field = DateTime.Now
+            };
             m_sopEventPendingConsoleRows.Add(pendingConsole);
 
-            var pendingWwp = new Adapters.Oal.dbo_event_pending_console();
-            pendingWwp.id = GenerateId(20);
-            pendingWwp.event_id = wwpParentId;
-            pendingWwp.event_class = "pos.oal.WwpEventNewLog";
-            pendingWwp.console_no = consignmentNo;
-            pendingWwp.version = 0;
-            pendingWwp.date_field = DateTime.Now;
+            var pendingWwp = new Adapters.Oal.dbo_event_pending_console
+            {
+                id = GenerateId(20),
+                event_id = wwpParentId,
+                event_class = "pos.oal.WwpEventNewLog",
+                console_no = consignmentNo,
+                version = 0,
+                date_field = DateTime.Now
+            };
             m_sopEventPendingConsoleRows.Add(pendingWwp);
         }
 
@@ -197,114 +186,7 @@ namespace Bespoke.PosEntt.CustomActions
             m_sopWwpEventLogRows.Add(wwp);
         }
 
-        public override string GetEditorViewModel()
-        {
-            return @"
-define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
-    function(context, logger, dialog, system) {
-
-                bespoke.sph.domain.SopWithChildrenAction = function(optionOrWebid) {
-
-                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
-                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.SopWithChildrenAction, rts.pickup.babies"";
-                    if (optionOrWebid && typeof optionOrWebid === ""object"")
-                    {
-                        for (var n in optionOrWebid)
-                        {
-                            if (typeof v[n] === ""function"")
-                            {
-                                v[n](optionOrWebid[n]);
-                            }
-                        }
-                    }
-                    if (optionOrWebid && typeof optionOrWebid === ""string"")
-                    {
-                        v.WebId(optionOrWebid);
-                    }
-
-                    if (bespoke.sph.domain.SopWithChildrenActionPartial)
-                    {
-                        return _(v).extend(new bespoke.sph.domain.SopWithChildrenActionPartial(v));
-                    }
-                    return v;
-                };
 
 
-
-        const action = ko.observable(new bespoke.sph.domain.SopWithChildrenAction(system.guid())),
-            trigger = ko.observable(),                   
-            activate = function() {
-                   return true;
-
-                },
-            attached = function(view) {
-                },
-            okClick = function(data, ev) {
-                    if (bespoke.utils.form.checkValidity(ev.target))
-                    {
-                        dialog.close(this, ""OK"");
-                    }
-                },
-            cancelClick = function() {
-                    dialog.close(this, ""Cancel"");
-                };
-
-                const vm = {
-                    trigger: trigger,
-                    action: action,
-                    activate: activate,
-                    attached: attached,
-                    okClick: okClick,
-                    cancelClick: cancelClick
-                };
-
-
-            return vm;
-
-        });
-";
-        }
-
-        public override string GetEditorView()
-        {
-            //language=html
-            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
-    <div class=""modal-dialog"">
-        <div class=""modal-content"">
-
-            <div class=""modal-header"">
-                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
-                <h3>RTS SOP with child items</h3>
-            </div>
-            <div class=""modal-body"" data-bind=""with: action"">
-
-                <form class=""form-horizontal"" id=""messaging-dialog-form"">
-
-
-                </form>
-            </div>
-            <div class=""modal-footer"">
-                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
-                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
-            </div>
-        </div>
-    </div>
-</section>";
-
-            return html;
-        }
-
-        private string GenerateId(int length)
-        {
-            var id = string.Format("en{0}", System.Guid.NewGuid().ToString("N"));            
-            return id.Substring(0, length);
-        }
-
-        private bool IsConsole(string connoteNo)
-        {
-            var pattern = "CG[0-9]{9}MY";
-            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, pattern);
-            return match.Success;
-        }
     }
 }
