ac38be4c9c5d97dda812c1ea353c84bd5545881c
erymuzuan <erymuzuan@hotmail.com>
2016-12-12 09:09:14 +0800

    Use the EventWithChildAction base class
    Related Work Items: #5394

--- sources/rts.pickup.babies/MissortWithChildrenAction.cs
+++ sources/rts.pickup.babies/MissortWithChildrenAction.cs
@@ -5,15 +5,13 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using Bespoke.Sph.Domain;
 using Polly;
-using System.Linq;
 
 namespace Bespoke.PosEntt.CustomActions
 {
     [Export(typeof(CustomAction))]
     [DesignerMetadata(Name = "MissWithChildren", TypeName = "Bespoke.PosEntt.CustomActions.MissWithChildrenAction, rts.pickup.babies", Description = "RTS MISSORT with child items", FontAwesomeIcon = "calendar-check-o")]
-    public class MissWithChildrenAction : CustomAction
+    public class MissWithChildrenAction : EventWithChildrenAction
     {
-        public override bool UseAsync => true;
         private List<Adapters.Oal.dbo_missort_event_new> m_missEventRows;
         private List<Adapters.Oal.dbo_event_pending_console> m_missEventPendingConsoleRows;
 
@@ -37,16 +35,15 @@ namespace Bespoke.PosEntt.CustomActions
             if (IsConsole(miss.ConsignmentNo)) consoleList.Add(miss.ConsignmentNo);
 
             var missEventAdapter = new Adapters.Oal.dbo_missort_event_newAdapter();
-            var missWwpEventAdapter = new Adapters.Oal.dbo_wwp_event_new_logAdapter();
             var missEventMap = new Integrations.Transforms.RtsMissToOalMissortEventNew();
             var parentRow = await missEventMap.TransformAsync(miss);
             parentRow.id = GenerateId(34);
             m_missEventRows.Add(parentRow);
             
-            var consoleItem = await SearchConsoleDetails(miss.ConsignmentNo);
+            var consoleItem = await GetItemConsigmentsFromConsoleDetailsAsync(miss.ConsignmentNo);
             if (null != consoleItem)
             {
-                var children = consoleItem.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                var children = consoleItem.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                 foreach (var item in children)
                 {
                     if (consoleList.Contains(item)) continue;
@@ -57,10 +54,10 @@ namespace Bespoke.PosEntt.CustomActions
                     if (console)
                     {
                         consoleList.Add(item);
-                        var childConsole = await SearchConsoleDetails(item);
+                        var childConsole = await GetItemConsigmentsFromConsoleDetailsAsync(item);
                         if (null != childConsole)
                         {
-                            var childConsoleItems = childConsole.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                            var childConsoleItems = childConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                             foreach (var cc in childConsoleItems)
                             {
                                 if (consoleList.Contains(cc)) continue;
@@ -71,10 +68,10 @@ namespace Bespoke.PosEntt.CustomActions
                                 if (anotherConsole)
                                 {
                                     consoleList.Add(cc);
-                                    var anotherChildConsole = await SearchConsoleDetails(cc);
+                                    var anotherChildConsole = await GetItemConsigmentsFromConsoleDetailsAsync(cc);
                                     if (null != anotherChildConsole)
                                     {
-                                        var anotherChildConsoleItems = anotherChildConsole.item_consignments.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                        var anotherChildConsoleItems = anotherChildConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
                                         foreach (var ccc in anotherChildConsoleItems)
                                         {
                                             if (consoleList.Contains(ccc)) continue;
@@ -124,13 +121,7 @@ namespace Bespoke.PosEntt.CustomActions
             }            
         }
 
-        async private Task<Adapters.Oal.dbo_console_details> SearchConsoleDetails(string consignmentNo)
-        {
-            var consoleDetailsAdapter = new Adapters.Oal.dbo_console_detailsAdapter();
-            var query = string.Format("SELECT * FROM [dbo].[console_details] WHERE console_no = '{0}'", consignmentNo);
-            var lo = await consoleDetailsAdapter.LoadAsync(query);
-            return lo.ItemCollection.FirstOrDefault();
-        }
+   
 
         private void ProcessChild(Adapters.Oal.dbo_missort_event_new parent, string consignmentNo)
         {
@@ -146,124 +137,18 @@ namespace Bespoke.PosEntt.CustomActions
         private void AddPendingItems(string parentId, string consignmentNo)
         {
             //insert into pending items
-            var pendingConsole = new Adapters.Oal.dbo_event_pending_console();
-            pendingConsole.id = GenerateId(20);
-            pendingConsole.event_id = parentId;
-            pendingConsole.event_class = "pos.oal.MissortEventNew";
-            pendingConsole.console_no = consignmentNo;
-            pendingConsole.version = 0;
-            pendingConsole.date_field = DateTime.Now;
+            var pendingConsole = new Adapters.Oal.dbo_event_pending_console
+            {
+                id = GenerateId(20),
+                event_id = parentId,
+                event_class = "pos.oal.MissortEventNew",
+                console_no = consignmentNo,
+                version = 0,
+                date_field = DateTime.Now
+            };
             m_missEventPendingConsoleRows.Add(pendingConsole);
         }
 
-        public override string GetEditorViewModel()
-        {
-            return @"
-define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
-    function(context, logger, dialog, system) {
-
-                bespoke.sph.domain.MissWithChildrenAction = function(optionOrWebid) {
-
-                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
-                    v[""$type""] = ""Bespoke.PosEntt.CustomActions.MissWithChildrenAction, rts.pickup.babies"";
-                    if (optionOrWebid && typeof optionOrWebid === ""object"")
-                    {
-                        for (var n in optionOrWebid)
-                        {
-                            if (typeof v[n] === ""function"")
-                            {
-                                v[n](optionOrWebid[n]);
-                            }
-                        }
-                    }
-                    if (optionOrWebid && typeof optionOrWebid === ""string"")
-                    {
-                        v.WebId(optionOrWebid);
-                    }
-
-                    if (bespoke.sph.domain.MissWithChildrenActionPartial)
-                    {
-                        return _(v).extend(new bespoke.sph.domain.MissWithChildrenActionPartial(v));
-                    }
-                    return v;
-                };
-
-
-
-        const action = ko.observable(new bespoke.sph.domain.MissWithChildrenAction(system.guid())),
-            trigger = ko.observable(),                   
-            activate = function() {
-                   return true;
-
-                },
-            attached = function(view) {
-                },
-            okClick = function(data, ev) {
-                    if (bespoke.utils.form.checkValidity(ev.target))
-                    {
-                        dialog.close(this, ""OK"");
-                    }
-                },
-            cancelClick = function() {
-                    dialog.close(this, ""Cancel"");
-                };
-
-                const vm = {
-                    trigger: trigger,
-                    action: action,
-                    activate: activate,
-                    attached: attached,
-                    okClick: okClick,
-                    cancelClick: cancelClick
-                };
-
-
-            return vm;
-
-        });
-";
-        }
-
-        public override string GetEditorView()
-        {
-            //language=html
-            var html = @"<section class=""view-model-modal"" id=""messaging-action-dialog"">
-    <div class=""modal-dialog"">
-        <div class=""modal-content"">
-
-            <div class=""modal-header"">
-                <button type=""button"" class=""close"" data-dismiss=""modal"" data-bind=""click : cancelClick"">&times;</button>
-                <h3>RTS MISSORT with child items</h3>
-            </div>
-            <div class=""modal-body"" data-bind=""with: action"">
-
-                <form class=""form-horizontal"" id=""messaging-dialog-form"">
-
-
-                </form>
-            </div>
-            <div class=""modal-footer"">
-                <input form=""messaging-dialog-form"" data-dismiss=""modal"" type=""submit"" class=""btn btn-default"" value=""OK"" data-bind=""click: okClick"" />
-                <a href=""#"" class=""btn btn-default"" data-dismiss=""modal"" data-bind=""click : cancelClick"">Cancel</a>
-            </div>
-        </div>
-    </div>
-</section>";
-
-            return html;
-        }
-
-        private string GenerateId(int length)
-        {
-            var id = string.Format("en{0}", System.Guid.NewGuid().ToString("N"));
-            return id.Substring(0, length);
-        }
-
-        private bool IsConsole(string connoteNo)
-        {
-            var pattern = "CG[0-9]{9}MY";
-            var match = System.Text.RegularExpressions.Regex.Match(connoteNo, pattern);
-            return match.Success;
-        }
+      
     }
 }
