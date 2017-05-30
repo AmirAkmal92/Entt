4c24565a77fc931e72ab0659f4de16fed941fc9b
Amir Akmal Hashim <amir@bespoke.com.my>
2017-05-30 14:12:20 +0800

    edited self service. add token search

--- web/SphApp/viewmodels/rts-self-service.js
+++ web/SphApp/viewmodels/rts-self-service.js
@@ -2,13 +2,26 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
     function (context, logger, router, app, validation) {
         var isBusy = ko.observable(false),
         ItemNo = ko.observable(""),
+        ScannerId = ko.observable(""),
+        AuthToken = ko.observableArray(),
         SearchItemNoResults = ko.observableArray(),
         BeatNo = ko.observable(""),
         CourierId = ko.observable(""),
         ReportDate = ko.observable(""),
         SearchByBeatNoAndCourierIdDeliResults = ko.observableArray(),
         SearchByBeatNoAndCourierIdPickResults = ko.observableArray(),
+        partial = partial || {},
+        list = ko.observableArray([]),
+        map = function (v) {
+            if (typeof partial.map === "function") {
+                return partial.map(v);
+            }
+            return v;
+        },
         activate = function (id) {
+            if (typeof partial.activate === "function") {
+                return partial.activate(list);
+            }
             return true;
         },
         searchItemNo = function () {
@@ -31,9 +44,34 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
                     });
             }
         },
+        searchScannerId = function () {
+            if (!$("#search-auth-form").valid()) {
+                return;
+            }
+            if (ScannerId() != "") {
+                isBusy(true);
+                context.get(`/api/auth-tokens/_search?q=${ScannerId()}`)
+                    .then(function (result) {
+                        isBusy(false);
+                        AuthToken(result.ItemCollection);
+                  
+                    }, function (e) {
+                        if (e.status == 422) {
+                            console.log("Unprocessable Entity");
+                        }
+                        isBusy(false);
+                    }).done(function () {
+                        $("#search-auth-token").text(ScannerId())
+                    });
+            }
+        },
         clearSearchItemNo = function () {
             ItemNo("");
             SearchItemNoResults.removeAll();
+        },
+        clearsearchScannerId = function () {
+            ScannerId("");
+            searchScannerId.removeAll();
         },
         searchByBeatNoAndCourierIdDeli = function () {
             searchByBeatNoAndCourierId("delivery", SearchByBeatNoAndCourierIdDeliResults);
@@ -121,6 +159,7 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
             ReportDate("");
             resultsArray.removeAll();
         },
+
         attached = function (view) {
             $("#ReportDateDeli").kendoDatePicker({
                 format: "yyyy-MM-dd"
@@ -138,6 +177,16 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
                     ItemNo: "Please provide valid consignment number or console tag."
                 }
             });
+            $("#search-auth-form").validate({
+                rules: {
+                    ScannerId: {
+                        required: true
+                    }
+                },
+                messages: {
+                    ScannerId: "Please provide valid scanner id."
+                }
+            })
             $("#delivery-report-form").validate({
                 rules: {
                     BeatNo: {
@@ -174,16 +223,25 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
                     ReportDatePick: "Please provide valid Date."
                 }
             });
+            if (typeof partial.attached === "function") {
+                partial.attached(view);
+            }
         },
         compositionComplete = function () {
 
         };
         return {
             isBusy: isBusy,
+            //query: query,
+            map: map,
             ItemNo: ItemNo,
+            ScannerId: ScannerId,
+            AuthToken: AuthToken,
             SearchItemNoResults: SearchItemNoResults,
+            searchScannerId: searchScannerId,
             searchItemNo: searchItemNo,
             clearSearchItemNo: clearSearchItemNo,
+            clearsearchScannerId: clearsearchScannerId,
             BeatNo: BeatNo,
             CourierId: CourierId,
             ReportDate: ReportDate,
@@ -195,6 +253,8 @@ define(["services/datacontext", "services/logger", "plugins/router", objectbuild
             clearBeatNoAndCourierIdPick: clearBeatNoAndCourierIdPick,
             activate: activate,
             attached: attached,
+            partial: partial,
+            list: list,
             compositionComplete: compositionComplete
         };
     });
