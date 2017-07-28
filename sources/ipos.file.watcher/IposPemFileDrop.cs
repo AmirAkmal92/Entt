fc9e27d56205cc3a9cb4b0efa7c58c2938fa7f75
azrolmn <azrolmn@gmail.com>
2017-07-28 17:07:00 +0800

    ipos pem file watcher tool

---
+++ sources/ipos.file.watcher/IposPemFileDrop.cs
@@ -0,0 +1,110 @@
+﻿using Bespoke.Sph.Domain;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Bespoke.PosEntt.Ipos.FileWatcher
+{
+    public class IposPemFileDrop : IReceiveLocation, IDisposable
+    {
+        private FileSystemWatcher m_watcher;
+        private bool m_paused;
+
+        private async void FswChanged(object sender, FileSystemEventArgs e)
+        {
+            if (m_paused) return;
+
+            var file = e.FullPath;
+            var wip = file + ".wip";
+            await WaitReadyAsync(file);
+
+            File.Move(file, wip);
+            await WaitReadyAsync(wip);
+            await Task.Delay(100);
+
+            var collectionFolder = ConfigurationManager.AppSettings["CentralLocation"];
+            if (!string.IsNullOrWhiteSpace(collectionFolder) && Directory.Exists(collectionFolder))
+            {
+                File.Copy(wip, Path.Combine(collectionFolder, Path.GetFileName(file)), true);
+                Console.WriteLine($"File [{Path.GetFileName(file)}] copied to central.");
+            }
+
+            var archivedFolder = ConfigurationManager.AppSettings["ArchiveLocation"];
+            if (!string.IsNullOrWhiteSpace(archivedFolder) && Directory.Exists(archivedFolder))
+            {
+                File.Move(wip, Path.Combine(archivedFolder, Path.GetFileName(file)));
+                Console.WriteLine($"File [{Path.GetFileName(file)}] archived.");
+            }
+            else
+                File.Delete(wip);
+        }
+
+        public async Task WaitReadyAsync(string fileName)
+        {
+            while (true)
+            {
+                try
+                {
+                    using (Stream stream = File.Open(fileName, FileMode.Open, FileAccess.ReadWrite, FileShare.ReadWrite))
+                    {
+                        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+                        if (stream != null)
+                        {
+                            Console.WriteLine($"Output file {Path.GetFileName(fileName)} ready.");
+                            break;
+                        }
+                    }
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine($"Output file {fileName} not yet ready ({ex.Message})");
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine($"Output file {fileName} not yet ready ({ex.Message})");
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine($"Output file {fileName} not yet ready ({ex.Message})");
+                }
+                await Task.Delay(500);
+            }
+        }
+
+        public void Dispose()
+        {
+            m_watcher?.Dispose();
+            m_watcher = null;
+        }
+
+        public void Pause()
+        {
+            Console.WriteLine($"File listening is paused.");
+            m_paused = true;
+        }
+
+        public void Resume()
+        {
+            Console.WriteLine($"File listening is resumed.");
+            m_paused = false;
+        }
+
+        public bool Start()
+        {
+            var path = ConfigurationManager.AppSettings["DropLocation"] ?? @"C:\temp\ipos-drop";
+            var filter = ConfigurationManager.AppSettings["FileFilter"] ?? @"*.txt*";
+            m_watcher = new FileSystemWatcher(path, filter) { EnableRaisingEvents = true };
+            m_watcher.Created += FswChanged;
+
+            Console.WriteLine($"Started file listening in [{path}] for any file drop...");
+            return true;
+        }
+
+        public bool Stop()
+        {
+            this.Dispose();
+            Console.WriteLine($"File listening is stopped.");
+            return true;
+        }
+    }
+}
---
+++ sources/ipos.file.watcher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Bespoke.Sph.Domain;
+using Topshelf;
+using Bespoke.PosEntt.Ipos.FileWatcher;
+
+namespace Bespoke.PosEntt.FileWatcher
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HostFactory.Run(config =>
+            {
+                config.UseNLog();
+                config.Service<IReceiveLocation>(svc =>
+                {
+                    svc.ConstructUsing(() => new IposPemFileDrop());
+
+                    svc.WhenStarted(x => x.Start());
+                    svc.WhenStopped(x => x.Stop());
+                    svc.WhenPaused(x => x.Pause());
+                    svc.WhenContinued(x => x.Resume());
+                    svc.WhenShutdown(x => x.Stop());
+                    svc.WhenCustomCommandReceived((x, g, i) =>
+                    {
+                        Console.WriteLine(g);
+                        Console.WriteLine(i);
+                    });
+                });
+                config.SetServiceName("Rx IPOS File Watcher");
+                config.SetDisplayName("Rx IPOS File Watcher");
+                config.SetDescription("IPOS file watcher for EnTT Acceptance Platform");
+
+                config.StartAutomatically();
+            });
+        }
+    }
+}
