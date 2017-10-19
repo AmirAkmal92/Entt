eb626c96e147ae5be72cb47c0669f77f69b5700b
Amir Akmal Hashim <amir@bespoke.com.my>
2017-10-19 11:55:23 +0800

    entt table for hip hop sip sop vasn

---
+++ database/Table/Entt.Sip.sql
@@ -0,0 +1,23 @@
+CREATE TABLE [Entt].[Sip](
+	[Id] [varchar](50) NOT NULL,
+	[Version] [int] NOT NULL,
+	[EventName] [varchar](50) NULL,/*EventRemarkDisplay*/
+	[Channel] [varchar](3) NULL,
+	[physical_channel] [varchar](2) NULL,
+	[Comment] [varchar](50) NULL,
+	[DateTime] SMALLDATETIME Not NULL,
+  [OfficeNo] [varchar](4) NULL,
+	[OfficeName] [varchar](30) NULL,
+	[OfficeNextCode] [varchar](4) NULL,
+	[BeatNo] [varchar](3) NULL,
+	[CourierId] [varchar](15) NULL,
+	[CourierName] [varchar](30) NULL,
+	[ItemTypeCode] [varchar](2) NULL,
+	[ConsignmentNo] [varchar](40) NULL,
+	[BatchName] [varchar](60) NULL,
+	[DataFlag] [varchar](1) NULL,
+	[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+	[CreatedBy] VARCHAR(50) NULL,
+	[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+	[ChangedBy] VARCHAR(50) NULL
+)
---
+++ database/Table/Entt.Sop.sql
@@ -0,0 +1,24 @@
+CREATE TABLE [Entt].[Sop](
+	[Id] [varchar](50) NOT NULL,
+	[Version] [int] NOT NULL,
+	[EventName] [varchar](50) NULL, /*EventRemarkDisplay*/
+	[Comment] [varchar](250) NULL,
+	[Channel] [varchar](3) NULL,
+	[DateTime] SMALLDATETIME NOT NULL,
+	[OfficeNo] [varchar](4) NULL,
+	[OfficeName] [varchar](30) NULL,
+	[OfficeNextCode] [varchar](4) NULL,
+	[BeatNo] [varchar](3) NULL,
+	[ConsignmentNo] [varchar](40) NULL,
+	[CourierId] [varchar](15) NULL,
+	[CourierName] [varchar](30) NULL,
+	[ItemTypeCode] [varchar](2) NULL,
+	[DestOfficeId] [varchar](4) NULL,
+	[DestOfficeName] [varchar](100) NULL,
+	[BatchName] [varchar](50) NULL,
+	[DataFlag] [varchar](1) NULL,
+	[CreatedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+	[CreatedBy] VARCHAR(50) NULL,
+	[ChangedDate] SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+	[ChangedBy] VARCHAR(50) NULL
+)
