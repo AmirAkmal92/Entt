12e643395dee54554ec0e7e3040638acbc8d1a7e
Amir Akmal Hashim <amir@bespoke.com.my>
2018-01-18 10:32:46 +0800

    hip and hop child for ers

---
+++ sources/entt.acceptance.customactions/HipWithChildrenAction.cs
@@ -0,0 +1,221 @@
+﻿using Bespoke.PosEntt.Adapters.Entt;
+using Bespoke.PosEntt.Integrations.Transforms;
+using Bespoke.PosEntt.RecordHips.Domain;
+using Bespoke.Sph.Domain;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Entt.Acceptance.CustomActions
+{
+    [Export(typeof(CustomAction))]
+    [DesignerMetadata(Name = "HipWithChildrenAction", TypeName = "Entt.Acceptance.CustomActions.HipWithChildrenAction,entt.acceptance.customactions", Description = "Entt HIP with child items", FontAwesomeIcon = "euro")]
+    public class HipWithChildrenAction : EventWithChildrenAction
+    {
+        private List<Entt_Hip> m_hipEventRows;
+        private List<Entt_EventPendingConsole> m_hipEventPendingConsoleRows;
+
+        public override async Task ExecuteAsync(RuleContext context)
+        {
+            var hip = context.Item as RecordHip;
+            if (null == hip) return;
+            var isConsole = IsConsole(hip.ConsignmentNo);
+            if (!isConsole) return;
+            
+            var swca = new HipWithChildrenAction()
+            {
+                m_hipEventRows = new List<Entt_Hip>(),
+                m_hipEventPendingConsoleRows = new List<Entt_EventPendingConsole>()
+            };
+            await swca.RunAsync(hip);
+        }
+
+        public async Task RunAsync(RecordHip hip)
+        {
+            //console_details
+            var consoleList = new List<string>();
+            if (IsConsole(hip.ConsignmentNo)) consoleList.Add(hip.ConsignmentNo);
+            
+            var hipEventMap = new RecordHipToEnttHip();
+            var hipConsoleRow = await hipEventMap.TransformAsync(hip);
+            hipConsoleRow.Id = GenerateId(34);
+            m_hipEventRows.Add(hipConsoleRow);
+
+            var consoleItem = await GetItemConsigmentsFromConsoleDetailsAsync(hip.ConsignmentNo);
+            if (null != consoleItem)
+            {
+                var children = consoleItem.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in children)
+                {
+                    if (consoleList.Contains(item)) continue;
+                    ProcessChild(hipConsoleRow, item);
+
+                    //2 level
+                    var console = IsConsole(item);
+                    if (console)
+                    {
+                        consoleList.Add(item);
+                        var childConsole = await GetItemConsigmentsFromConsoleDetailsAsync(item);
+                        if (null != childConsole)
+                        {
+                            var childConsoleItems = childConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var cc in childConsoleItems)
+                            {
+                                if (consoleList.Contains(cc)) continue;
+                                ProcessChild(hipConsoleRow, cc);
+
+                                //3 level
+                                var anotherConsole = IsConsole(cc);
+                                if (anotherConsole)
+                                {
+                                    consoleList.Add(cc);
+                                    var anotherChildConsole = await GetItemConsigmentsFromConsoleDetailsAsync(cc);
+                                    if (null != anotherChildConsole)
+                                    {
+                                        var anotherChildConsoleItems = anotherChildConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                        foreach (var ccc in anotherChildConsoleItems)
+                                        {
+                                            if (consoleList.Contains(ccc)) continue;
+                                            ProcessChild(hipConsoleRow, ccc);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        AddPendingItems(hipConsoleRow.Id, cc);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            AddPendingItems(hipConsoleRow.Id, item);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                AddPendingItems(hipConsoleRow.Id, hip.ConsignmentNo);
+            }
+
+            var hipEventAdapter = new Entt_HipAdapter();
+            foreach (var item in m_hipEventRows)
+            {
+                var pr = Policy.Handle<SqlException>()
+                    .WaitAndRetryAsync(5, x => TimeSpan.FromMilliseconds(500 * Math.Pow(2, x)))
+                    .ExecuteAndCaptureAsync(() => hipEventAdapter.InsertAsync(item));
+                var result = await pr;
+                if (result.FinalException != null)
+                    throw result.FinalException; // send to dead letter queue
+                System.Diagnostics.Debug.Assert(result.Result > 0, "Should be at least 1 row");
+            }
+
+            var pendingAdapter = new Entt_EventPendingConsoleAdapter();
+            foreach (var item in m_hipEventPendingConsoleRows)
+            {
+                var pr = Policy.Handle<SqlException>()
+                    .WaitAndRetryAsync(5, x => TimeSpan.FromMilliseconds(500 * Math.Pow(2, x)))
+                    .ExecuteAndCaptureAsync(() => pendingAdapter.InsertAsync(item));
+                var result = await pr;
+                if (result.FinalException != null)
+                    throw result.FinalException; // send to dead letter queue
+                System.Diagnostics.Debug.Assert(result.Result > 0, "Should be at least 1 row");
+            }
+        }
+
+        private void ProcessChild(Entt_Hip parent, string consignmentNo)
+        {
+            var isConsole = IsConsole(consignmentNo);
+            var child = parent.Clone();
+            child.Id = GenerateId(34);
+            child.ConsignmentNo = consignmentNo;
+            child.DataFlag = "1";
+            child.ItemTypeCode = isConsole ? "02" : "01";
+            m_hipEventRows.Add(child);
+        }
+
+        private void AddPendingItems(string parentId, string consignmentNo)
+        {
+            var pendingConsole = new Entt_EventPendingConsole
+            {
+                Id = GenerateId(20),
+                EventId = parentId,
+                EventName = "Hip",
+                ConsoleNo = consignmentNo,
+                Version = 0,
+                DateTime = DateTime.Now
+            };
+            m_hipEventPendingConsoleRows.Add(pendingConsole);
+        }
+
+
+        public override string GetEditorViewModel()
+        {
+            return @"
+define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
+    function(context, logger, dialog, system) {
+
+                bespoke.sph.domain.HipWithChildrenAction = function(optionOrWebid) {
+
+                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
+                    v[""$type""] = ""Entt.Acceptance.CustomActions.HipWithChildrenAction,entt.acceptance.customactions"";
+                    if (optionOrWebid && typeof optionOrWebid === ""object"")
+                    {
+                        for (var n in optionOrWebid)
+                        {
+                            if (typeof v[n] === ""function"")
+                            {
+                                v[n](optionOrWebid[n]);
+                            }
+                        }
+                    }
+                    if (optionOrWebid && typeof optionOrWebid === ""string"")
+                    {
+                        v.WebId(optionOrWebid);
+                    }
+
+                    if (bespoke.sph.domain.HipWithChildrenActionPartial)
+                    {
+                        return _(v).extend(new bespoke.sph.domain.HipWithChildrenActionPartial(v));
+                    }
+                    return v;
+                };
+
+
+
+        const action = ko.observable(new bespoke.sph.domain.HipWithChildrenAction(system.guid())),
+            trigger = ko.observable(),                   
+            activate = function() {
+                   return true;
+
+                },
+            attached = function(view) {
+                },
+            okClick = function(data, ev) {
+                    if (bespoke.utils.form.checkValidity(ev.target))
+                    {
+                        dialog.close(this, ""OK"");
+                    }
+                },
+            cancelClick = function() {
+                    dialog.close(this, ""Cancel"");
+                };
+
+                const vm = {
+                    trigger: trigger,
+                    action: action,
+                    activate: activate,
+                    attached: attached,
+                    okClick: okClick,
+                    cancelClick: cancelClick
+                };
+            return vm;
+
+        });
+";
+        }
+    }
+}
---
+++ sources/entt.acceptance.customactions/HopWithChildrenAction.cs
@@ -0,0 +1,222 @@
+﻿using Bespoke.PosEntt.Adapters.Entt;
+using Bespoke.PosEntt.Integrations.Transforms;
+using Bespoke.PosEntt.RecordHops.Domain;
+using Bespoke.Sph.Domain;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Entt.Acceptance.CustomActions
+{
+    [Export(typeof(CustomAction))]
+    [DesignerMetadata(Name = "HopWithChildrenAction", TypeName = "Entt.Acceptance.CustomActions.HopWithChildrenAction,entt.acceptance.customactions", Description = "Entt HOP with child items", FontAwesomeIcon = "euro")]
+    public class HopWithChildrenAction : EventWithChildrenAction
+    {
+        private List<Entt_Hop> m_hopEventRows;
+        private List<Entt_EventPendingConsole> m_hopEventPendingConsoleRows;
+
+        public override async Task ExecuteAsync(RuleContext context)
+        {
+            var hop = context.Item as RecordHop;
+            if (null == hop) return;
+            var isConsole = IsConsole(hop.ConsignmentNo);
+            if (!isConsole) return;
+
+            var swca = new HopWithChildrenAction()
+            {
+                m_hopEventRows = new List<Entt_Hop>(),
+                m_hopEventPendingConsoleRows = new List<Entt_EventPendingConsole>()
+            };
+            await swca.RunAsync(hop);
+        }
+
+        public async Task RunAsync(RecordHop hop)
+        {
+            //console_details
+            var consoleList = new List<string>();
+            if (IsConsole(hop.ConsignmentNo)) consoleList.Add(hop.ConsignmentNo);
+
+            var hopEventMap = new RecordHopToEnttHop();
+            var hopConsoleRow = await hopEventMap.TransformAsync(hop);
+            hopConsoleRow.Id = GenerateId(34);
+            m_hopEventRows.Add(hopConsoleRow);
+
+            var consoleItem = await GetItemConsigmentsFromConsoleDetailsAsync(hop.ConsignmentNo);
+            if (null != consoleItem)
+            {
+                var children = consoleItem.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in children)
+                {
+                    if (consoleList.Contains(item)) continue;
+                    ProcessChild(hopConsoleRow, item);
+
+                    //2 level
+                    var console = IsConsole(item);
+                    if (console)
+                    {
+                        consoleList.Add(item);
+                        var childConsole = await GetItemConsigmentsFromConsoleDetailsAsync(item);
+                        if (null != childConsole)
+                        {
+                            var childConsoleItems = childConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var cc in childConsoleItems)
+                            {
+                                if (consoleList.Contains(cc)) continue;
+                                ProcessChild(hopConsoleRow, cc);
+
+                                //3 level
+                                var anotherConsole = IsConsole(cc);
+                                if (anotherConsole)
+                                {
+                                    consoleList.Add(cc);
+                                    var anotherChildConsole = await GetItemConsigmentsFromConsoleDetailsAsync(cc);
+                                    if (null != anotherChildConsole)
+                                    {
+                                        var anotherChildConsoleItems = anotherChildConsole.Split(new[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                                        foreach (var ccc in anotherChildConsoleItems)
+                                        {
+                                            if (consoleList.Contains(ccc)) continue;
+                                            ProcessChild(hopConsoleRow, ccc);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        AddPendingItems(hopConsoleRow.Id, cc);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            AddPendingItems(hopConsoleRow.Id, item);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                AddPendingItems(hopConsoleRow.Id, hop.ConsignmentNo);
+            }
+
+            var sipEventAdapter = new Entt_HopAdapter();
+            foreach (var item in m_hopEventRows)
+            {
+                var pr = Policy.Handle<SqlException>()
+                    .WaitAndRetryAsync(5, x => TimeSpan.FromMilliseconds(500 * Math.Pow(2, x)))
+                    .ExecuteAndCaptureAsync(() => sipEventAdapter.InsertAsync(item));
+                var result = await pr;
+                if (result.FinalException != null)
+                    throw result.FinalException; // send to dead letter queue
+                System.Diagnostics.Debug.Assert(result.Result > 0, "Should be at least 1 row");
+            }
+
+            var pendingAdapter = new Entt_EventPendingConsoleAdapter();
+            foreach (var item in m_hopEventPendingConsoleRows)
+            {
+                var pr = Policy.Handle<SqlException>()
+                    .WaitAndRetryAsync(5, x => TimeSpan.FromMilliseconds(500 * Math.Pow(2, x)))
+                    .ExecuteAndCaptureAsync(() => pendingAdapter.InsertAsync(item));
+                var result = await pr;
+                if (result.FinalException != null)
+                    throw result.FinalException; // send to dead letter queue
+                System.Diagnostics.Debug.Assert(result.Result > 0, "Should be at least 1 row");
+            }
+        }
+
+
+        private void ProcessChild(Entt_Hop parent, string consignmentNo)
+        {
+            var isConsole = IsConsole(consignmentNo);
+            var child = parent.Clone();
+            child.Id = GenerateId(34);
+            child.ConsignmentNo = consignmentNo;
+            child.DataFlag = "1";
+            child.ItemTypeCode = isConsole ? "02" : "01";
+            m_hopEventRows.Add(child);
+        }
+
+        private void AddPendingItems(string parentId, string consignmentNo)
+        {
+            var pendingConsole = new Entt_EventPendingConsole
+            {
+                Id = GenerateId(20),
+                EventId = parentId,
+                EventName = "Hop",
+                ConsoleNo = consignmentNo,
+                Version = 0,
+                DateTime = DateTime.Now
+            };
+            m_hopEventPendingConsoleRows.Add(pendingConsole);
+        }
+
+
+        public override string GetEditorViewModel()
+        {
+            return @"
+define([""services/datacontext"", 'services/logger', 'plugins/dialog', objectbuilders.system],
+    function(context, logger, dialog, system) {
+
+                bespoke.sph.domain.HopWithChildrenAction = function(optionOrWebid) {
+
+                    const v = new bespoke.sph.domain.CustomAction(optionOrWebid);
+                    v[""$type""] = ""Entt.Acceptance.CustomActions.HopWithChildrenAction,entt.acceptance.customactions"";
+                    if (optionOrWebid && typeof optionOrWebid === ""object"")
+                    {
+                        for (var n in optionOrWebid)
+                        {
+                            if (typeof v[n] === ""function"")
+                            {
+                                v[n](optionOrWebid[n]);
+                            }
+                        }
+                    }
+                    if (optionOrWebid && typeof optionOrWebid === ""string"")
+                    {
+                        v.WebId(optionOrWebid);
+                    }
+
+                    if (bespoke.sph.domain.HopWithChildrenActionPartial)
+                    {
+                        return _(v).extend(new bespoke.sph.domain.HopWithChildrenActionPartial(v));
+                    }
+                    return v;
+                };
+
+
+
+        const action = ko.observable(new bespoke.sph.domain.HopWithChildrenAction(system.guid())),
+            trigger = ko.observable(),                   
+            activate = function() {
+                   return true;
+
+                },
+            attached = function(view) {
+                },
+            okClick = function(data, ev) {
+                    if (bespoke.utils.form.checkValidity(ev.target))
+                    {
+                        dialog.close(this, ""OK"");
+                    }
+                },
+            cancelClick = function() {
+                    dialog.close(this, ""Cancel"");
+                };
+
+                const vm = {
+                    trigger: trigger,
+                    action: action,
+                    activate: activate,
+                    attached: attached,
+                    okClick: okClick,
+                    cancelClick: cancelClick
+                };
+            return vm;
+
+        });
+";
+        }
+    }
+}
